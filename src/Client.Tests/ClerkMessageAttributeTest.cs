57c7135b082bc26e999d591c361818f286b363f9
Julien Blin <julien.blin@gmail.com>
2011-02-05 16:17:40 -0500

    Added ClerkMessageAttribute with string interpolation.

---
+++ src/Client.Tests/ClerkMessageAttributeTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Colombo.Clerk.Client.Tests
+{
+    [TestFixture]
+    public class ClerkMessageAttributeTest
+    {
+        [Test]
+        public void It_should_return_a_basic_message()
+        {
+            var msg = new ClerkMessageAttribute("Foo");
+
+            Assert.That(msg.GetRealMessage(null, null), Is.EqualTo("Foo"));
+        }
+
+        [Test]
+        public void It_should_do_string_interpolation_with_simple_message()
+        {
+            var msg = new ClerkMessageAttribute("$request.FirstName become $response.Name.");
+
+            Assert.That(
+                msg.GetRealMessage(
+                    new TestRequest { FirstName = "Foo" },
+                    new TestResponse { Name = "Bar" }
+                ),
+                Is.EqualTo("Foo become Bar."));
+        }
+
+        [Test]
+        public void It_should_allow_string_interpolation_with_methods()
+        {
+            var msg = new ClerkMessageAttribute("His name is $request.GetName().");
+
+            Assert.That(
+                msg.GetRealMessage(
+                    new TestRequest { FirstName = "Foo", LastName = "Bar" },
+                    null
+                ),
+                Is.EqualTo("His name is Foo Bar."));
+        }
+
+        [Test]
+        public void It_should_return_resources()
+        {
+            var msg = new ClerkMessageAttribute("SimpleTest", typeof (TestsResourceClerkMessage));
+
+            Assert.That(msg.GetRealMessage(null, null), Is.EqualTo("Something"));
+        }
+
+        [Test]
+        public void It_should_do_string_interpolation_with_resources()
+        {
+            var msg = new ClerkMessageAttribute("InterpolTest", typeof(TestsResourceClerkMessage));
+
+            Assert.That(
+                msg.GetRealMessage(
+                    new TestRequest { LastName = "Foo" },
+                    new TestResponse { Name = "Bar" }
+                ),
+                Is.EqualTo("Name is Foo for Bar."));
+        }
+
+        [Test]
+        public void It_should_not_throw_exception_if_resource_not_found()
+        {
+            var msg = new ClerkMessageAttribute("NoRes", typeof(TestsResourceClerkMessage));
+
+            Assert.That(msg.GetRealMessage(null, null), Is.Null);
+        }
+
+        public class TestResponse : Response
+        {
+            public string Name { get; set; }
+        }
+
+        public class TestRequest : Request<TestResponse>
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string GetName()
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
---
+++ src/Client/ClerkMessageAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using NVelocity;
+using NVelocity.App;
+
+namespace Colombo.Clerk.Client
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ClerkMessageAttribute : Attribute
+    {
+        static ClerkMessageAttribute()
+        {
+            Velocity.Init();
+        }
+
+        public ClerkMessageAttribute(string message)
+        {
+            Message = message;
+        }
+
+        public ClerkMessageAttribute(string messageResourceName, Type messageResourceType)
+        {
+            MessageResourceName = messageResourceName;
+            MessageResourceType = messageResourceType;
+        }
+
+        public string Message { get; private set; }
+
+        public string MessageResourceName { get; private set; }
+
+        public Type MessageResourceType { get; private set; }
+
+        public string GetRealMessage(BaseRequest request, Response response)
+        {
+            var baseMessage = GetBaseMessage();
+            if (baseMessage == null)
+                return null;
+
+            var context = new VelocityContext();
+            context.Put("request", request);
+            context.Put("response", response);
+
+            var result = new StringWriter();
+
+            Velocity.Evaluate(context, result, "", baseMessage);
+            return result.ToString();
+        }
+
+        public string GetBaseMessage()
+        {
+            if (MessageResourceType == null)
+                return Message;
+
+            return new ResourceManager(MessageResourceType).GetString(MessageResourceName);
+        }
+    }
+}
