465b0c424c9d9fd39266a49426e41deae032e1ed
Julien Blin <julien.blin@gmail.com>
2011-02-20 17:07:47 -0500

    Added GestDistinctValuesRequest.

---
+++ src/Messages/GetDistinctValuesRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Colombo.Clerk.Messages
+{
+    public class GetDistinctValuesRequest : SideEffectFreeRequest<GetDistinctValuesResponse>
+    {
+        public GetDistinctValueType ValueType { get; set; }
+    }
+
+    public enum GetDistinctValueType
+    {
+        RequestNamespace,
+        RequestType,
+        ResponseNamespace,
+        ResponseType,
+        ContextKey
+    }
+}
---
+++ src/Messages/GetDistinctValuesResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Colombo.Clerk.Messages
+{
+    public class GetDistinctValuesResponse : Response
+    {
+        public GetDistinctValuesResponse()
+        {
+            Values = new List<string>();
+        }
+
+        public virtual IEnumerable<string> Values { get; set; }
+    }
+}
