07673b599cffa43fbae820706ca2edee1280a5b7
Julien Blin <julien.blin@gmail.com>
2011-02-22 13:45:57 -0500

    Added unobtrusive templating.

---
+++ src/Web/Content/JS/Clerk.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('div[data-remote="true"]').each(function (index, element) {
+        var jqElement = $(element);
+        var elementId = element.id;
+        var url = jqElement.attr('data-url');
+        var tmplId = jqElement.attr('data-template');
+        $.ajax({
+            url: url,
+            type: 'GET',
+            contentType: 'application/json',
+            success: function (data, status, xhr) {
+                $("#" + tmplId).tmpl(data).appendTo("#" + elementId);
+            },
+            error: function (xhr, status, error) {
+                alert(error);
+            }
+        });
+    });
+});
